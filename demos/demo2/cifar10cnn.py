afa2371e094265b32b53466dbf9e26b9e1c635e1
Jun Khai <moltenhound@gmail.com>
2023-09-13 01:44:51 +1000

    finished all questions and reorganised

---
+++ demos/demo2/cifar10cnn.py
@@ -0,0 +1,225 @@
+'''
+CNN for classifying CIFAR10
+'''
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU")
+
+if __name__ == '__main__':
+    # Hyper-parameters
+    num_epochs = 35
+    learning_rate = 0.05
+    num_classes = 10
+    gen_plot = False
+
+
+    #paths
+    model_name = "resnet"
+    path = '/home/Student/s4581696/'
+    path = '.' 
+    batch_size = 32
+
+    transform_train = transforms.Compose([
+        transforms.ToTensor(), 
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010),),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
+        ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(), 
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010),)
+        ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./cifar10', train=True,
+                                            download=True, transform=transform_train)
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./cifar10', train=False,
+                                        download=True, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+    class BasicBlock(nn.Module):
+        expansion = 1
+        
+        def __init__(self, in_planes, planes, stride=1):
+            super(BasicBlock, self).__init__()
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn2 = nn.BatchNorm2d(planes)
+            
+            self.shortcut = nn.Sequential()
+            if stride != 1 or in_planes != self.expansion*planes:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(self.expansion*planes)
+                )
+            
+        def forward(self, x):
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.bn2(self.conv2(out))
+            out += self.shortcut(x)
+            out = F.relu(out)
+            return out
+
+    class ResNet(nn.Module):
+        def __init__(self, block, num_blocks, num_classes=10):
+            super(ResNet, self).__init__()
+            
+            self.in_planes = 64
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(64)
+            self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+            self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+            self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+            self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+            self.linear = nn.Linear(512*block.expansion, num_classes)
+
+        def _make_layer(self, block, planes, num_blocks, stride):
+            strides = [stride] + [1]*(num_blocks-1)
+            layers = []
+            for stride in strides:
+                layers.append(block(self.in_planes, planes, stride))
+                self.in_planes = planes * block.expansion
+
+            return nn.Sequential(*layers)
+        
+        
+        def forward(self, x): #forward pass of the model
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.layer1(out)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = self.layer4(out)
+            out = F.avg_pool2d(out, 4)
+            out = out.view(out.size(0), -1) #view as 1D
+            out = self.linear(out)
+            return out
+        
+    def ResNet18():
+        return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+    model = ResNet18()
+    model = model.to(device)
+
+    print("Model No. of Parameters:", sum([param.nelement() for param in model.parameters()]))
+    print(model)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0005)
+
+    total_steps = len(train_loader)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, total_steps=total_steps)
+    
+    train_acc = []
+    test_acc = []
+    
+    model.train()
+    start = time.time() #time generation
+    loop = tqdm(range(num_epochs), ncols=110)
+    for epoch in loop:
+        total_steps = len(train_loader)
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, total_steps=total_steps)
+        for i, (images, labels) in enumerate(train_loader): #load a batch
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loop.set_postfix_str("Epoch [{}/{}], Step [{}/{}] Loss: {:.5f}"
+                        .format(epoch+1, num_epochs, i+1, total_steps, loss.item()))
+            scheduler.step()
+        
+        if gen_plot:
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for images, labels in train_loader:
+                    images = images.to(device)
+                    labels = labels.to(device)
+
+                    outputs = model(images)
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                    
+                train_acc.append(correct / total)
+                print('Train Accuracy: {} %'.format(100 * correct / total))
+                
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for images, labels in test_loader:
+                    images = images.to(device)
+                    labels = labels.to(device)
+
+                    outputs = model(images)
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                    
+                test_acc.append(correct / total)
+                print('Test Accuracy: {} %'.format(100 * correct / total))
+
+    end = time.time()
+    elapsed = end - start
+    print("Training took " + str(elapsed) + " secs or " + str(elapsed/60) + " mins in total") 
+
+    # Test the model
+    print("> Testing")
+    start = time.time() #time generation
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print('Test Accuracy: {} %'.format(100 * correct / total))
+        
+    end = time.time()
+    elapsed = end - start
+    print("Testing took " + str(elapsed) + " secs or " + str(elapsed/60) + " mins in total")
+    
+    if gen_plot:
+        plt.plot(train_acc, label="Train Acc")
+        
+        plt.plot(test_acc, label="Test Acc")
+        plt.title("Test vs Train Accuracies for CIFAR10")
+        plt.xlabel("No. epochs")
+        plt.xlabel("Accuracy (%)")
+        plt.legend()
+        plt.savefig("./cifar10cnn50.png")
+
+    
+    torch.save(model.state_dict(), "./cifar10_model_state.pth")
---
+++ demos/demo2/job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=p100
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=cifarcnn
+#SBATCH --output=cifarcnn.out
+
+source /home/Student/s4581696/.bashrc
+conda activate /home/Student/s4581696/coco/envs/torch2
+export PYTHONPATH="/home/Student/s4581696/slurm"
+echo $PYTHONPATH
+python3 ./lec.py
